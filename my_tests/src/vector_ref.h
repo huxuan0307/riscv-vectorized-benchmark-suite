a86d2334f472ed534ad1db055d76ccc6b2ff9048
hx_rust <532391012@qq.com>
2021-12-05 23:14:29 +0800

    fix compile error

--- my_tests/src/vector_ref.h
+++ my_tests/src/vector_ref.h
@@ -8,7 +8,7 @@ void vfadd_vv_f64_vec(double* vd, double* vs2, double* vs1, int n) {
     
     for (i = 0; i < n;) {
         // gvl = __builtin_epi_vsetvl(n - i, __epi_e64, __epi_m1);
-        gvl = vsetvl_e64m1(n - i)
+        gvl = vsetvl_e64m1(n - i);
         __epi_1xf64 v_vs1 = vle64_v_f64m1(&vs1[i]);
         __epi_1xf64 v_vs2 = vle64_v_f64m1(&vs2[i]);
 
